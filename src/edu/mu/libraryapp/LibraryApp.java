edc34e8cc156b747ea2fc674a579bc741c5434b7
WoodySmith2004 <bsmith_04@yahoo.com>
2025-02-11 22:28:54 -0600

    commit two

--- src/edu/mu/libraryapp/LibraryApp.java
+++ src/edu/mu/libraryapp/LibraryApp.java
@@ -4,7 +4,6 @@ public class LibraryApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//according to all known laws of aviation, bees should not be able to fly, but bees do not care about human laws
 	}
 
 }
